a24983e4d0b64d4e90f34fcaa22f48144ab45344
borey <artemmaya4ok@gmail.com>
2025-09-06 09:01:50 +0300

    Working on entries yay

--- server/internal/routes/api.go
+++ server/internal/routes/api.go
@@ -39,7 +39,7 @@ func SetupAPIRoutes(
 			protected.POST("/tasks", taskHandler.CreateTask) // Create a task
 			//
 			// // protected.GET("/requirements/entries", taskHandler.GetRequirements) // GET /requirements/entries?start=2024-01-01T00:00:00&end=2024-01-31T23:59:59
-			// protected.POST("/requirements/:requirement_id/entries") // Create an entry for any requirement
+			protected.POST("/requirements/:requirement_id/entries") // Create an entry for any requirement
 			// protected.GET("/requirements/:requirement_id/entries")  // Get all entries, make it with date start and end
 			// protected.GET("/entries/:entry_id")                     // Get specific entry
 			// protected.PUT("/entries/:entry_id")                     // Update entry
