f8a47175b63c8b0ef2525814a3447c344405e3f1
Borey <borey@noreply.com>
2026-06-28 23:39:53 +0300

    Added tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { heyApiPlugin } from '@hey-api/vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     heyApiPlugin(),
     vue(),
     vueDevTools(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
